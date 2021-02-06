0c028eabbda564c7afe8da4bd5d39d3dfb7834ca
SepidehZV <zamanvaziri.sepideh@gmail.com>
2021-02-06 14:22:24 -0500

    done with server

--- server/routes/path.js
+++ server/routes/path.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
+// helper function for directory
+const hierarchy = (directory, parent = ["root"]) => {
+  console.log(directory);
+  const keys = {};
+  if (parent.length === 1) {
+    if (directory.type === "file") {
+      return `THIS IS FILE : ${parent}`;
+    } else {
+      for (key in directory.children) {
+        keys[key] = true;
+      }
+      return keys;
+    }
+  }
+  let childs;
+  for (i = 0; i < parent.length - 1; i++) {
+    childs = hierarchy(directory, [parent[i]]);
+    if (childs[parent[i + 1]]) {
+      console.log("HOME", parent[i + 1]);
+      parent.shift();
+      childs = hierarchy(directory.children[parent[i]], parent);
+    }
+  }
+  return childs;
+};
+
 /* GET users listing. */
 module.exports = ({ directory }) => {
-  router.get("/:id", function (req, res, next) {
-    const fullUrl = req.url;
-    res.send(fullUrl);
+  router.get("/", function (req, res, next) {
+    let keys = ["root"];
+    if (req.query) {
+      for (key in req.query) {
+        keys.push(key);
+      }
+    }
+    res.json(hierarchy(directory, keys));
   });
 
   return router;
